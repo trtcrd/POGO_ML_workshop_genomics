341cef642e502080759b77973db4b8b3e50dbead
lanzen <anders.lanzen@gmail.com>
2019-05-17 14:44:18 +0200

    Small fixes to tutorial part 2

--- genephene_install.sh
+++ genephene_install.sh
@@ -1,4 +1,5 @@
 sudo apt install python3-pandas python3-sklearn emacs gedit
 wget https://www.dropbox.com/s/ksduhn8hvyfz65o/genephene.tar.gz
 tar xvzf genephene.tar.gz
+rm genephene.tar.gz
 mv genephene $HOME
